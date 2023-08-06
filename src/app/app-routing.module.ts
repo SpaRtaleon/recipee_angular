a1fbaa7610ffd25c70816a59156aa89071e980b1
SpaRtaleon <raghubalan2972001@gmail.com>
2023-08-06 19:40:33 +0530

    first

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './categories/categories.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  { path: 'category', component: CategoriesComponent },
+  { path: '', component: HomeComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/recipee.spec.ts
@@ -0,0 +1,7 @@
+import { Recipee } from './recipee';
+
+describe('Recipee', () => {
+  it('should create an instance', () => {
+    expect(new Recipee()).toBeTruthy();
+  });
+});
