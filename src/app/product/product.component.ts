65d1cbd9faaf956384ef8b2ce7484aef9e4f651e
SpaRtaleon <raghubalan2972001@gmail.com>
2023-09-15 23:16:30 +0530

    product page

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -11,13 +11,19 @@ export class ProductComponent implements OnInit{
 
      id:any;
      recipe :any;
+     ingredients:any;
+     instructions!:string;
+     directions:string[]=[];
      ngOnInit() {
       this.route.params.subscribe(params => {
         const id = params['id']; 
         console.log(id);
         if(id){
-          this.apiService.getRecipe(id).subscribe((data)=>{
-            this.recipe=data;
+          this.apiService.getRecipe(id).subscribe((data:any)=>{
+            this.recipe=data.recipe;
+            this.ingredients=data.ingredients;
+            this.instructions=data.recipe.Recipe_Procedure;
+            this.directions=this.instructions.split(".")
              console.log(this.recipe,"recipe");
            });
         }
