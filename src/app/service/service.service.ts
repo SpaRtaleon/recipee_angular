39bf86771ea9aa1cfe0646d43658c7b77c551ac8
SpaRtaleon <raghubalan2972001@gmail.com>
2023-09-19 10:47:28 +0530

    service url

--- src/app/service/service.service.ts
+++ src/app/service/service.service.ts
@@ -13,7 +13,8 @@ export class ServiceService {
 
 
 
- private  apiUrl="http://localhost:8000"
+//  private  apiUrl="http://localhost:8000"
+    private  apiUrl="https://recipee-angular.vercel.app/"
 
 
 
